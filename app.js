b1f611f1c25a91fba368f3cbb1e9535c1883cb23
estefano99 <estefanobugari@gmail.com>
2022-07-22 18:27:38 -0300

    correcciones varias

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const navbar = document.querySelector('.navbar');
 const nav_link = document.querySelector('.nav-link-imagen');
 const nav_image = document.querySelector('.nav-imagen');
 const cta_tienda = document.querySelector('.link-botton-banner');
+const logo_hereford = document.querySelector('.logo-hereford');
 
 // Event Listeners
 
@@ -26,14 +27,20 @@ cta_tienda.addEventListener('click', (e) => {
 
 /* Funciones */
 function setSticky() {
-    navbar.style.lineHeight = '11vh';
+    /* navbar.style.lineHeight = '100px'; */
+    navbar.classList.add('navbar-chica')
+    /* logo_hereford.style.width = '6%' */
+    logo_hereford.classList.add('logo-hereford-chico')
     nav_link.classList.remove('nav-link-imagen-chica');
     nav_link.classList.add('nav-link-imagen-chica');
     navbar.style.backgroundColor = '#012b3b';
 }
 
 function setFixed() {
-    navbar.style.lineHeight = '14vh';
+    /* navbar.style.lineHeight = '150px'; */
+    navbar.classList.remove('navbar-chica')
+    /* logo_hereford.style.width = '10%'; */
+    logo_hereford.classList.remove('logo-hereford-chico')
     nav_link.classList.remove('nav-link-imagen-chica');
     nav_link.classList.add('nav-link-imagen');
     navbar.style.backgroundColor = '#012c3bf5';
