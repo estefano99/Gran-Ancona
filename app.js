e2ee600284a12db6c245b9ca564372791aa5d915
JuliBosch <julibosch99@gmail.com>
2022-09-11 20:53:32 -0300

    favicon, fuentes y divs

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const navbar = document.querySelector('.navbar');
 const nav_link = document.querySelector('.nav-link-imagen');
 const nav_image = document.querySelector('.nav-imagen');
-const cta_tienda = document.querySelector('.link-botton-banner');
 const logo_hereford = document.querySelector('.logo-hereford');
 const logo_cabana = document.querySelector('.logo-cabana');
 const btn_fijo_wpp = document.querySelector('.btn-fijo-wpp');
@@ -25,35 +24,28 @@ window.addEventListener('scroll', () => {
     }
 })
 
-cta_tienda.addEventListener('click', (e) => {
-
-    setTimeout(() => {
-        window.open('https://mercado.genganar.com/es/84_gran-ancona', '_blank');
-    }, 1000);
-})
-
 /* Funciones */
-
 function reconocerDispositivo() {
     //Para activar el icono de wpp para celulares
     if (x <= 500) {
         btn_fijo_wpp.style.display = "none";
         btn_celu_wpp.style.display = "block";
 
+        // El texto cambia dependiendo de si es celu o pc
         carrusel_texto.innerHTML = '<p>A continuación se presentan imágenes de nuestros animales, logros, y de quienes forman parte de <strong>Gran Ancona SA.</strong></p>';
     }else{
         btn_fijo_wpp.style.display = "block";
         btn_celu_wpp.style.display = "none";
 
+        // El texto cambia dependiendo de si es celu o pc
         carrusel_texto.innerHTML = '<p>Aquí se presentan imágenes de nuestros animales, logros, y de quienes forman parte de <strong>Gran Ancona SA.</strong></p>';
     }
 
 }
 
 function setSticky() {
-    /* navbar.style.lineHeight = '100px'; */
     navbar.classList.add('navbar-chica');
-    /* logo_hereford.style.width = '6%' */
+
     logo_hereford.classList.add('logo-hereford-chico');
     logo_cabana.classList.add('logo-cabana-chico');
 
@@ -63,9 +55,8 @@ function setSticky() {
 }
 
 function setFixed() {
-    /* navbar.style.lineHeight = '150px'; */
     navbar.classList.remove('navbar-chica');
-    /* logo_hereford.style.width = '10%'; */
+
     logo_hereford.classList.remove('logo-hereford-chico');
     logo_cabana.classList.remove('logo-cabana-chico');
 
